c2608a54040c90be4c8a5e7c40c9a67a5fbaab4c
MandarEkal <155659402+GR4V1TY123@users.noreply.github.com>
2024-08-30 14:55:05 +0000

    /

--- models/mines.js
+++ models/mines.js
@@ -5,11 +5,35 @@ const mineSchema = new Schema({
     name: String,
     state: String,
     district: String,
-    footprint: Number,
-    coalType: String,
-    qty: Number,
     image: String,
-    description: String
+    description: String,
+    footprint: {
+        directEmission: Number,
+        electricityUse: Number,
+        trnasport: Number,
+        deforestation: Number,
+        total: Number
+    },
+    factors: {
+        coalQty: Number,
+        coalType: {
+            lignite: Number,
+            subbitu: Number,
+            bitu: Number,
+            anthra: Number
+        },
+        electricityConsume: Number,
+        eFactor: Number,
+        fuelUse: {
+            diesel: Number,
+            petrol: Number,
+            dfueleff: Number,
+            pfueleff:Number
+        },
+        employeeCount: Number,
+        deforestedArea: Number,
+        cStock: Number
+    }
 })
 
 module.exports = mongoose.model('Mine', mineSchema);
