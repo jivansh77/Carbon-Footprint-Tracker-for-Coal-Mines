a66ab57ef14097ac9bd79bdc30acd16c5661e4b1
MandarEkal <155659402+GR4V1TY123@users.noreply.github.com>
2024-08-31 16:10:32 +0000

    home

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ app.engine('ejs', ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static('pages'));
 
 mongoose.connect('mongodb://localhost:27017/mines', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -27,6 +28,59 @@ app.set('views', path.join(__dirname, 'pages'));
 
 app.get("/home", (req, res) => {
     res.render("home");
+})
+
+app.get("/addmine", (req,res) =>{
+    res.render("addmine", {places});
+})
+
+app.post("/index", async(req,res) =>{
+    const mineData = req.body.Mine;
+    const mine = new Mines(mineData);  
+    await mine.save();
+    res.redirect("/index");
+})
+
+app.get("/index", async(req,res) =>{
+    const mines = await Mines.find({});
+    res.render("index", {mines});
+})
+
+app.get("/calculate", async(req,res) =>{
+    res.render("calculate" , {places});
+})
+
+app.get("/index/:id", async(req,res) =>{
+    const mines = await Mines.findById(req.params.id);
+    res.render("show", {mines});
+})
+
+app.delete("/index/:id", async(req,res)=>{
+    const mines = await Mines.findByIdAndDelete(req.params.id)
+    res.redirect("/index");
+})
+
+app.put("/index/:id", async(req,res)=>{
+    const mines = await Mines.findByIdAndUpdate(req.params.id,req.body.Mine)
+    res.redirect(`/index/${mines.id}`);
+})
+
+app.get("/index/:id/edit", async(req,res) =>{
+    const mines = await Mines.findById(req.params.id);
+    res.render("edit", {mines , places});
+})
+
+
+
+
+
+
+
+app.get('/getCities', (req, res) => {
+    const state = req.query.state;
+    const selectedState = places.states.find(s => s.name === state);
+    const cities = selectedState ? selectedState.districts.map(d => d.name) : [];
+    res.json(cities);
 });
 
 app.get("/addmine", (req, res) => {
