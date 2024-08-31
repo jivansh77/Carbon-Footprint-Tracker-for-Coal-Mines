cc52118daa1d3d573c95427fc40f8f5f22a733d0
jivansh77 <jivansh777@gmail.com>
2024-08-31 19:59:45 +0530

    added error handling

--- main.js
+++ main.js
@@ -1,89 +1,128 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 const methodOverride = require('method-override');
-const mongoose = require("mongoose");
-const Mines = require("./models/mines");
+const mongoose = require('mongoose');
+const Mines = require('./models/mines');
 const ejsMate = require('ejs-mate');
-const places = require("./data/india-places");
+const places = require('./data/india-places');
 
 app.engine('ejs', ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-mongoose.connect('mongodb://localhost:27017/mines')
+mongoose.connect('mongodb://localhost:27017/mines', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("MONGO CONNECTION OPEN!!!")
+        console.log("MONGO CONNECTION OPEN!!!");
     })
     .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!!")
-        console.log(err)
-    })
+        console.log("OH NO MONGO CONNECTION ERROR!!!!");
+        console.log(err);
+    });
 
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'pages'));
 
-app.get("/home", (req,res) =>{
+app.get("/home", (req, res) => {
     res.render("home");
-})
-
-app.get("/addmine", (req,res) =>{
-    res.render("addmine", {places});
-})
-
-app.post("/index", async(req,res) =>{
-    const mineData = req.body.Mine;
-    const mine = new Mines(mineData);  
-    await mine.save();
-    res.redirect("/index");
-})
-
-app.get("/index", async(req,res) =>{
-    const mines = await Mines.find({});
-    res.render("index", {mines});
-})
-
-app.get("/calculate", async(req,res) =>{
-    res.render("calculate");
-})
-
-app.get("/index/:id", async(req,res) =>{
-    const mines = await Mines.findById(req.params.id);
-    res.render("show", {mines});
-})
-
-app.delete("/index/:id", async(req,res)=>{
-    const mines = await Mines.findByIdAndDelete(req.params.id)
-    res.redirect("/index");
-})
-
-app.put("/index/:id", async(req,res)=>{
-    const mines = await Mines.findByIdAndUpdate(req.params.id,req.body.Mine)
-    res.redirect(`/index/${mines.id}`);
-})
-
-app.get("/index/:id/edit", async(req,res) =>{
-    const mines = await Mines.findById(req.params.id);
-    res.render("edit", {mines , places});
-})
-
-
-
-
-
-
-
-app.get('/getCities', (req, res) => {
-    const state = req.query.state;
-    const selectedState = places.states.find(s => s.name === state);
-    const cities = selectedState ? selectedState.districts.map(d => d.name) : [];
-    res.json(cities);
 });
 
+app.get("/addmine", (req, res) => {
+    res.render("addmine", { places });
+});
 
+app.post("/index", async (req, res) => {
+    try {
+        const mineData = req.body.Mine;
+        const mine = new Mines(mineData);
+        await mine.save();
+        res.redirect("/index");
+    } catch (err) {
+        console.error("Error adding mine:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
-app.listen("3000", ()=>{
+app.get("/index", async (req, res) => {
+    try {
+        const mines = await Mines.find({});
+        res.render("index", { mines });
+    } catch (err) {
+        console.error("Error fetching mines:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.get("/calculate", (req, res) => {
+    res.render("calculate");
+});
+
+app.get("/index/:id", async (req, res) => {
+    try {
+        const mines = await Mines.findById(req.params.id);
+        if (!mines) {
+            return res.status(404).send("Mine not found");
+        }
+        res.render("show", { mines });
+    } catch (err) {
+        console.error("Error fetching mine:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.delete("/index/:id", async (req, res) => {
+    try {
+        const mines = await Mines.findByIdAndDelete(req.params.id);
+        if (!mines) {
+            return res.status(404).send("Mine not found");
+        }
+        res.redirect("/index");
+    } catch (err) {
+        console.error("Error deleting mine:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.put("/index/:id", async (req, res) => {
+    try {
+        const mines = await Mines.findByIdAndUpdate(req.params.id, req.body.Mine, { new: true });
+        if (!mines) {
+            return res.status(404).send("Mine not found");
+        }
+        res.redirect(`/index/${mines.id}`);
+    } catch (err) {
+        console.error("Error updating mine:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.get("/index/:id/edit", async (req, res) => {
+    try {
+        const mines = await Mines.findById(req.params.id);
+        if (!mines) {
+            return res.status(404).send("Mine not found");
+        }
+        res.render("edit", { mines, places });
+    } catch (err) {
+        console.error("Error fetching mine for edit:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.get('/getCities', (req, res) => {
+    try {
+        const state = req.query.state;
+        const selectedState = places.states.find(s => s.name === state);
+        const cities = selectedState ? selectedState.districts.map(d => d.name) : [];
+        res.json(cities);
+    } catch (err) {
+        console.error("Error fetching cities:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.listen("3000", () => {
     console.log("SERVER RUNNING.....................");
-})
+});
